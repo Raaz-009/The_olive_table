9a510f8e761a9b89ae989314be0f1fcfec27c939
Raaz <142300018+Raaz-009@users.noreply.github.com>
2026-09-10 11:55:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Story from "./components/Story";
 import Signature from "./components/Signature";
 import FullMenu from "./components/FullMenu";
 import Chef from "./components/Chef";
+import React from "react";
 import Gallery from "./components/Gallery";
 import Reviews from "./components/Reviews";
 import Reserve from "./components/Reserve";
